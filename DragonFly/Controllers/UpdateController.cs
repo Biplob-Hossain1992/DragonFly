4198dccd3c9e3ad83c292ae8a425d1dc7c608209
Biplob-Hossain1992 <biplobcse.diu@gmail.com>
2021-11-15 00:04:37 +0600

    UpdateController Created

---
+++ DragonFly/Controllers/UpdateController.cs
@@ -0,0 +1,24 @@
+﻿using DragonFly.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DragonFly.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Update")]
+    [Authorize]
+    [ApiController]
+    public class UpdateController : ControllerBase
+    {
+        private readonly IMembersInformationService _membersInformationService;
+        public UpdateController(IMembersInformationService membersInformationService)
+        {
+            _membersInformationService = membersInformationService;
+        }
+    }
+}
