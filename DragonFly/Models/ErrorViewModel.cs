5192db5ec148d5b9088f5a09ba2ab50f9cc8a24f
Biplob-Hossain1992 <biplobcse.diu@gmail.com>
2023-07-10 01:05:56 +0600

    Add project files.

---
+++ DragonFly/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DragonFly.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
